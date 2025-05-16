7eb1dc5ce38234a641ee0be421c46883a1c4edca
trainermags <60736918+Magesh-Tech-PM@users.noreply.github.com>
2025-05-16 11:19:54 +0100

    changes browser driver
    Update the browser drive and change the website address to google

--- src/test/java/helloworld_google.java
+++ src/test/java/helloworld_google.java
@@ -6,7 +6,7 @@ public class helloworld_google {
 
     public static void main(String[] args) {
 
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
 
         driver.get("http://www.google.com");
 
